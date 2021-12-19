8df7ab08ee272b4cd409e456064f937f57187db5
qianmian yu <im.qianmian.yu@gmail.com>
2021-12-19 17:23:08 +0800

    Add method that returns result in JSON.

--- src/query.rs
+++ src/query.rs
@@ -11,6 +11,12 @@ struct ParseResult {
 }
 
 #[derive(Debug, serde::Deserialize)]
+struct ParseJsonResult {
+    version: u32,
+    stmts: Vec<serde_json::Value>,
+}
+
+#[derive(Debug, serde::Deserialize)]
 struct Stmt {
     stmt: crate::ast::Node,
     stmt_len: Option<u32>,
@@ -55,6 +61,28 @@ pub fn parse(stmt: &str) -> Result<Vec<crate::ast::Node>> {
             serde_json::from_slice(raw.to_bytes()).map_err(|e| Error::InvalidAst(e.to_string()))?;
         pg_query_free_parse_result(result);
         Ok(parsed.stmts.into_iter().map(|s| s.stmt).collect())
+    }
+}
+
+pub fn parse_json(stmt: &str) -> Result<Vec<serde_json::Value>> {
+    unsafe {
+        let c_str = CString::new(stmt).unwrap();
+        let result = pg_query_parse(c_str.as_ptr() as *const c_char);
+
+        // Capture any errors first
+        if !result.error.is_null() {
+            let error = &*result.error;
+            let message = CStr::from_ptr(error.message).to_string_lossy().into();
+            pg_query_free_parse_result(result);
+            return Err(Error::ParseError(message));
+        }
+
+        // Parse the JSON into the AST
+        let raw = CStr::from_ptr(result.parse_tree);
+        let parsed: ParseJsonResult =
+            serde_json::from_slice(raw.to_bytes()).map_err(|e| Error::InvalidAst(e.to_string()))?;
+        pg_query_free_parse_result(result);
+        Ok(parsed.stmts)
     }
 }
 
